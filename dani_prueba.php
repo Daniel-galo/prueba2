598f12bc9c4415520ef901e2c48a8c6f9f251afb
Daniel <d.garcialopez2001@gmail.com>
2021-10-05 20:13:24 +0200

    Create dani_prueba.php

---
+++ dani_prueba.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Prueba2</title>
+    </head>
+    <body>
+
+            echo "¡Hola, soy un alumno de ASIX2!";
+        ?>
+
+    </body>
+</html>
